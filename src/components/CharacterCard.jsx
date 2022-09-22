518d33a92a2aebf86a98ec180bc575f16652506e
Valery Ilchenko <ilchenko.valery.developer@gmail.com>
2022-09-22 15:02:40 +0300

    added dependencies to popup

--- src/components/CharacterCard.jsx
+++ src/components/CharacterCard.jsx
@@ -1,67 +1,47 @@
 import React from "react";
 import {
-  Box,
   Card,
   CardActionArea,
   CardContent,
   CardMedia,
-  Modal,
-  Typography
+  Typography,
 } from "@mui/material";
+import CardModal from "./CardModal";
 
-const CharacterCard = ({ character }) => {
+const CharacterCard = ({
+  character: { image, name, gender, type, status, species },
+}) => {
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
 
-  const style = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: 400,
-    bgcolor: 'background.paper',
-    border: '2px solid #000',
-    boxShadow: 24,
-    p: 4,
-  };
-
   return (
     <>
-      <Card
-        sx={{ maxWidth: 345 }}
-        onClick={handleOpen}
-      >
+      <Card sx={{ maxWidth: 345 }} onClick={handleOpen}>
         <CardActionArea>
           <CardMedia
             component="img"
             height="140"
-            image={character?.image}
+            image={image}
             alt="green iguana"
           />
           <CardContent>
             <Typography gutterBottom variant="h5" component="div">
-              {character?.name}
+              {name}
             </Typography>
           </CardContent>
         </CardActionArea>
       </Card>
-
-      <Modal
+      <CardModal 
         open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
-        </Box>
-      </Modal>
+        handleClose={handleClose}
+        image={image}
+        name={name}
+        gender={gender}
+        type={type}
+        status={status}
+        species={species}
+      />
     </>
   );
 };
